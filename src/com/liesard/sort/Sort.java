99410641123879e3db3a279ed7f054a99275c24b
liesard <liesfromalizard@gmail.com>
2015-05-03 23:45:06 +0200

    Updated

--- src/com/liesard/sort/Sort.java
+++ src/com/liesard/sort/Sort.java
@@ -1,32 +1,43 @@
 package com.liesard.sort;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Sort {
-	public List<String> Sort(List<String> list) {
+	public static List<String> sort(List<String> list) {
 		List<String> sortedList = new ArrayList<String>();
+		String tmp;
+		Character c, d;
+		boolean swapped = false;
 
-		for (String currentWord : list) {
-			sortedList.add(currentWord);
+		while (!swapped) {
+			for (int index = 0; index < list.size() - 1; index++) {
+				c = list.get(index).charAt(0);
+				d = list.get(index + 1).charAt(0);
 
-			for (String otherWord : sortedList) {
-				if (currentWord.equals(otherWord)) {
+				if (c > d) {
+					if (index == list.size() - 1 && list.size() > 0) {
+						sortedList.add(0, list.get(index));
+						list.remove(index);
+						index = 0;
+					}
+					tmp = list.get(index + 1);
+					list.set(index + 1, list.get(index));
+					list.set(index, tmp);
+				} else if (c < d) {
 					continue;
-				} else {
-					System.out.println(currentWord);
 				}
 			}
-		}
-
-		for (String s : sortedList) {
-			System.out.println(s);
 		}
 
 		return sortedList;
 	}
 
 	public static void main(String[] args) {
-		new Sort(new List<String>(args));
+		List<String> arguments = Arrays.asList(args);
+		for (String s : sort(arguments)) {
+			System.out.println(s);
+		}
 	}
 }
