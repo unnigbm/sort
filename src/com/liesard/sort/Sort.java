220a28850a3f65b3bc5e13cabf1b3ffb656657c5
liesard <liesfromalizard@gmail.com>
2015-05-14 15:50:43 +0200

    Fixed bubble sort and allowed generating javadocs

--- src/com/liesard/sort/Sort.java
+++ src/com/liesard/sort/Sort.java
@@ -4,23 +4,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Sort: a library that sorts a string of text using various methods
+ * @author liesard
+ * @version 0.1
+ */
+
 public class Sort {
-    public static List<String> bubbleSort(List<String> list) {
-        String tmp;
-        Character currentChar, lastChar;
+    public Sort(List<String> list) {
+        bubbleSort(list);
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    /** Sorts a list of strings using bubble sort
+     *  @param list The list to be sorted
+     *  @return The sorted list
+     */
+    public List<String> bubbleSort(List<String> list) {
+        String currentWord, lastWord, tmp;
         boolean swapped = false;
+        int wordLength;
 
         for (int index = 1; index < list.size(); index++) {
-            currentChar = list.get(index).charAt(0);
-            lastChar = list.get(index - 1).charAt(0);
+            currentWord = list.get(index);
+            lastWord = list.get(index - 1);
 
-            if (currentChar < lastChar) {
-                tmp = list.get(index - 1);
-                list.set(index - 1, list.get(index));
-                list.set(index, tmp);
+            if (checkStrings(currentWord, lastWord)) {
+                swap(list, index, index - 1);
                 swapped = true;
             }
-            if (index == list.size() - 1) {
+
+            if (index >= list.size() - 1) {
                 if (!swapped) {
                     break;
                 } else {
@@ -31,5 +48,29 @@ public class Sort {
         }
 
         return list;
+    }
+
+    private boolean checkStrings(String string0, String string1) {
+        int stringLength = string0.length() < string1.length() ? string0.length() : string1.length();
+
+        for (int charPos = 0; charPos < stringLength; charPos++) {
+            if (string0.charAt(charPos) < string1.charAt(charPos)) {
+                return true;
+            } else if (string0.charAt(charPos) > string1.charAt(charPos)) {
+                break;
+            }
+        }
+
+        return false;
+    }
+
+    private void swap(List<String> list, int index0, int index1) {
+        String tmp = list.get(index0);
+        list.set(index0, list.get(index1));
+        list.set(index1, tmp);
+    }
+
+    public static void main(String[] args) {
+        new Sort(Arrays.asList(args));
     }
 }
